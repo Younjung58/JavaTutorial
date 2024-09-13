4883a18f2a8233a6cacac1409ad88acddb7fd610
Younjung58 <skykh999@naver.com>
2024-09-13 17:28:09 +0900

    보너스

--- 이클립스단축키/기본단축키.java
+++ 이클립스단축키/기본단축키.java
@@ -1,41 +1,42 @@
 package 이클립스단축키;
 
 public class 기본단축키 {
-//	단축키 목록 보기	Ctrl + shift + L	
-//	단어 완성	단어 일부 입력후, alt + /
-//	저장	Ctrl + S	
-//	자동 수정 (Quick fix)	Ctrl + 1
-//	실행	Ctrl + F11	
-//	같은 단어 표시（형광펜）	alt + shift + 0
-//	전체 선택	Ctrl + A	
-//	행으로 이동	Ctrl + L
-//	한 줄 삭제	Ctrl + D	
-//	최근 수정지점으로 이동	Ctrl + Q
-//	다음 단어 삭제	Ctrl + delete	
-//	소스 탭 간 이동	Ctrl + pgup, pgdn
-//	이전 단어 삭제	Ctrl + backspace	
-//	소스 탭 목록 보기	Ctrl + shift + E
-//	단어간 커서 이동	Ctrl + up, down	
-//	현재 소스 탭 닫기	Ctrl + F4
-//	찾기 / 바꾸기	Ctrl + F	
-//	리소스（파일） 찾기	Ctrl + shift 4- R
-//	검색	Ctrl + H	
-//	편집 이력 이동	alt + left, right
-//	주석 / 해제	Ctrl + /	
-//	편집창 폰트 크기	Ctrl + +, -
-//	범위 주석 / 해제	Ctrl + shift + /, W	
-//	속성 보기	alt + Enter
-//	멀티 컬럼 편집	Ctrl + A, shift + up, down	
-//	선언 보기	F3
-//	행 이동（여러 행 가능)	alt + up, down	
-//	상속 계층도 보기	클래스 이름 클릭, F4
-//	행 복사（여러 행 가능)	alt + Ctrl + shift + up, down	
-//	상속 계층도 보기	Ctrl + T
-//	자동 들여쓰기	Ctrl + i	
-//	경로 보기	alt + shift + B
-//	자동 형식 맞추기	Ctrl + shift + F	
-//	import문 자동 추가	Ctrl + shift + 0
-//	자동완성	Ctrl + space	
-//	멤버 목록 보기	Ctrl + 0
+	
+	//	단축키 목록 보기	Ctrl + shift + L	
+	//	단어 완성	단어 일부 입력후, alt + /
+	//	저장	Ctrl + S	
+	//	자동 수정 (Quick fix)	Ctrl + 1
+	//	실행	Ctrl + F11	
+	//	같은 단어 표시（형광펜）	alt + shift + 0
+	//	전체 선택	Ctrl + A	
+	//	행으로 이동	Ctrl + L
+	//	한 줄 삭제	Ctrl + D	
+	//	최근 수정지점으로 이동	Ctrl + Q
+	//	다음 단어 삭제	Ctrl + delete	
+	//	소스 탭 간 이동	Ctrl + pgup, pgdn
+	//	이전 단어 삭제	Ctrl + backspace	
+	//	소스 탭 목록 보기	Ctrl + shift + E
+	//	단어간 커서 이동	Ctrl + up, down	
+	//	현재 소스 탭 닫기	Ctrl + F4
+	//	찾기 / 바꾸기	Ctrl + F	
+	//	리소스（파일） 찾기	Ctrl + shift 4- R
+	//	검색	Ctrl + H	
+	//	편집 이력 이동	alt + left, right
+	//	주석 / 해제	Ctrl + /	
+	//	편집창 폰트 크기	Ctrl + +, -
+	//	범위 주석 / 해제	Ctrl + shift + /, W	
+	//	속성 보기	alt + Enter
+	//	멀티 컬럼 편집	Ctrl + A, shift + up, down	
+	//	선언 보기	F3
+	//	행 이동（여러 행 가능)	alt + up, down	
+	//	상속 계층도 보기	클래스 이름 클릭, F4
+	//	행 복사（여러 행 가능)	alt + Ctrl + shift + up, down	
+	//	상속 계층도 보기	Ctrl + T
+	//	자동 들여쓰기	Ctrl + i	
+	//	경로 보기	alt + shift + B
+	//	자동 형식 맞추기	Ctrl + shift + F	
+	//	import문 자동 추가	Ctrl + shift + 0
+	//	자동완성	Ctrl + space	
+	//	멤버 목록 보기	Ctrl + 0
 
 }
