9ded2cc3bc10ddbd30ca8597a11f4acb4c78add4
Younjung58 <skykh999@naver.com>
2024-09-20 15:56:45 +0900

    클래스와객체

--- _09_Class_Test/as/CarMain.java
+++ _09_Class_Test/as/CarMain.java
@@ -3,7 +3,7 @@ package _09_Class_Test.as;
 public class CarMain {
 
 	public static void main(String[] args) {
-		System.out.println("자동차관리프로그램 v1.6");
+		System.out.println("자동차관리프로그램 v1.0");
 		new CarCenter();
 	}
 
