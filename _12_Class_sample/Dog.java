d9cb3f04298029a8d3f954c29d9a17fd9a02d6a8
Younjung58 <skykh999@naver.com>
2024-09-25 11:18:07 +0900

    에러

---
+++ _12_Class_sample/Dog.java
@@ -0,0 +1,9 @@
+package _12_Class_sample;
+
+public class Dog {
+
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ _12_Class_sample/MyDogList.java
@@ -0,0 +1,10 @@
+package _12_Class_sample;
+
+public class MyDogList {
+
+	public MyDogList() {
+		// TODO Auto-generated constructor stub
+		System.out.println("--도그 관리--");
+	}
+
+}
