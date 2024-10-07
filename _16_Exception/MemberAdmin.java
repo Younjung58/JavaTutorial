e3df74fbaa9ddee830b770cdf41f7664b24285eb
Younjung58 <skykh999@naver.com>
2024-10-07 10:52:51 +0900

    예외처리

--- _16_Exception/MemberAdmin.java
+++ _16_Exception/MemberAdmin.java
@@ -1,5 +1,35 @@
 package _16_Exception;
 
-public class MemberAdmin {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class MemberAdmin {
+	MemberOne[] m = new MemberOne[5];
+	
+	MemberAdmin() {/*
+		System.out.println(m[0]);
+		System.out.println(m[1].getId());
+		System.out.println(m[2]);
+		System.out.println("close");*/
+		test();
+	}
+	private void test() {
+		File file = new File("Output.txt");
+		FileWriter writer;
+		try {
+			// try블록은 예외가 발생할 코드를 작성한다.
+			// 예외가 발생하면 더이상 코드를 실행하지 않고 catch블록으로 이동
+			writer = new FileWriter(file);
+		} catch (IOException e) {
+			// catch 소괄호안의 예외가 발생했을 때 처리해주는 블록
+			e.printStackTrace();
+		} finally {
+			// try구문을 빠져 나갈때 실행됨. 예외 발생 여부 상관없이 실행됨.
+		}
+		// 코드상의 빨간줄: 에러  => 예외처리가 필요하다는 에러
+		// try ~ catch ~ : 직접 예외처리하는 구문
+		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+	}
 }
--- _16_Exception/MemberOne.java
+++ _16_Exception/MemberOne.java
@@ -1,5 +1,14 @@
 package _16_Exception;
 
 public class MemberOne {
+	
+	private String id = null;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
